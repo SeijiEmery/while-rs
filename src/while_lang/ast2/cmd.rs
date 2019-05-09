ba8d02650780e250d81a21318c2f250c609d7a4f
Seiji Emery <seijiemery@gmail.com>
2019-05-09 00:38:22 -0700

    wrote reference eval() impl for cmd

--- src/while_lang/ast2/cmd.rs
+++ src/while_lang/ast2/cmd.rs
@@ -1,5 +1,5 @@
 use std::rc::Rc;
-use super::expr::Expr;
+use super::expr::{ CmdExpr, Expr };
 use super::state::{ State, Variable };
 use super::aexpr::{ ARef };
 use super::bexpr::{ BRef };
@@ -23,17 +23,44 @@ pub fn assign (v: &str, expr: ARef) -> CRef { Rc::new(Cmd::Assign(v.to_string(),
 pub fn if_ (cond: BRef, a: CRef, b: CRef) -> CRef { Rc::new(Cmd::If(cond, a, b)) }
 pub fn while_ (cond: BRef, body: CRef) -> CRef { Rc::new(Cmd::While(cond, body)) }
 
-impl Expr<bool, CRef> for CRef {
+impl CmdExpr<CRef> for CRef {
     fn is_reduced (&self) -> bool {
         return match **self {
             Cmd::Skip => true,
             _ => false
         }
     }
-    fn eval (&self, state: &State) -> Result<bool, String> {
-        Err("unimplemented!".to_string())
+    // for reference
+    fn eval (&self, state: &mut State) -> Result<(), String> {
+        match **self {
+            Cmd::Skip => Ok(()),
+            Cmd::Seq(ref a, ref b) => match a.eval(state) {
+                Ok(_) => match b.eval(state) {
+                    Ok(_) => Ok(()),
+                    err => err
+                }, err => err
+            },
+            Cmd::Assign(ref v, ref expr) => match expr.eval(state) {
+                Ok(a) => { state.set(v, a); Ok(()) }
+                Err(msg) => Err(msg)
+            },
+            Cmd::If(ref cond, ref a, ref b) => match cond.eval(state) {
+                Ok(true) => a.eval(state),
+                Ok(false) => b.eval(state),
+                Err(msg) => Err(msg)
+            },
+            Cmd::While(ref cond, ref body) => {
+                loop { // dangerous: an infinite loop will loop infinitely!
+                    match cond.eval(state) {
+                        Ok(true) => { body.eval(state); },
+                        Ok(false) => { return Ok(()) },
+                        Err(msg) => { return Err(msg) },
+                    }
+                }
+            }
+        }
     }
-    fn eval1 (&self, state: &State) -> CResult {
+    fn eval1 (&self, state: &mut State) -> CResult {
         Err("unimplemented!".to_string())
     }
 }
@@ -43,5 +70,9 @@ mod tests {
     use super::*;
     use super::super::state_mocks::*;
 
-    // Tests TBD
+    #[test]
+    fn test_skip () {
+        let state = MockEmptyState::new();
+        assert_eq!(true, skip().is_reduced());
+    }
 }
--- src/while_lang/ast2/expr.rs
+++ src/while_lang/ast2/expr.rs
@@ -6,3 +6,8 @@ pub trait Expr <Value, AST>: fmt::Debug {
     fn eval1 (&self, state: &State) -> Result<AST, String>;
     fn is_reduced (&self) -> bool;
 }
+pub trait CmdExpr <AST>: fmt::Debug {
+    fn eval(&self, state: &mut State) -> Result<(), String>;
+    fn eval1(&self, state: &mut State) -> Result<AST, String>;
+    fn is_reduced(&self) -> bool;
+}
