74911a8e750621cdac22654d160409a137837b97
Seiji Emery <seijiemery@gmail.com>
2019-05-09 01:27:29 -0700

    wrote impl for cmd with partial tests

--- src/while_lang/ast2/cmd.rs
+++ src/while_lang/ast2/cmd.rs
@@ -1,8 +1,8 @@
 use std::rc::Rc;
 use super::expr::{ CmdExpr, Expr };
 use super::state::{ State, Variable };
-use super::aexpr::{ ARef };
-use super::bexpr::{ BRef };
+use super::aexpr::{ ARef, AExpr };
+use super::bexpr::{ BRef, BExpr };
 use super::aexpr;
 use super::bexpr;
 
@@ -12,7 +12,7 @@ enum Cmd {
     Seq(CRef, CRef),
     Assign(Variable, ARef),
     If(BRef, CRef, CRef),
-    While(BRef, CRef),
+    While(BRef, BRef, CRef),
 }
 pub type CRef = Rc<Cmd>;
 pub type CResult = Result<CRef, String>;
@@ -21,7 +21,8 @@ pub fn skip () -> CRef { Rc::new(Cmd::Skip) }
 pub fn seq (a: CRef, b: CRef) -> CRef { Rc::new(Cmd::Seq(a, b)) }
 pub fn assign (v: &str, expr: ARef) -> CRef { Rc::new(Cmd::Assign(v.to_string(), expr)) }
 pub fn if_ (cond: BRef, a: CRef, b: CRef) -> CRef { Rc::new(Cmd::If(cond, a, b)) }
-pub fn while_ (cond: BRef, body: CRef) -> CRef { Rc::new(Cmd::While(cond, body)) }
+pub fn while_ (cond: BRef, body: CRef) -> CRef { Rc::new(Cmd::While(cond.clone(), cond, body)) }
+fn while_ev (cond: BRef, c0: BRef, body: CRef) -> CRef { Rc::new(Cmd::While(cond, c0, body)) }
 
 impl CmdExpr<CRef> for CRef {
     fn is_reduced (&self) -> bool {
@@ -49,7 +50,7 @@ impl CmdExpr<CRef> for CRef {
                 Ok(false) => b.eval(state),
                 Err(msg) => Err(msg)
             },
-            Cmd::While(ref cond, ref body) => {
+            Cmd::While(ref cond, ref c0, ref body) => {
                 loop { // dangerous: an infinite loop will loop infinitely!
                     match cond.eval(state) {
                         Ok(true) => { body.eval(state); },
@@ -61,18 +62,93 @@ impl CmdExpr<CRef> for CRef {
         }
     }
     fn eval1 (&self, state: &mut State) -> CResult {
-        Err("unimplemented!".to_string())
+        match **self {
+            Cmd::Skip => Ok(self.clone()),
+            Cmd::Seq(ref a, ref b) => match **a {
+                Cmd::Skip => b.eval1(state),
+                _ => a.eval1(state).map(|a| seq(a, b.clone())),
+            },
+            Cmd::Assign(ref v, ref expr) => match **expr {
+                AExpr::Val( a) => { state.set(v, a); Ok(skip()) },
+                _ => expr.eval1(state).map(|expr| assign(&v, expr)),
+            },
+            Cmd::If(ref cond, ref a, ref b) => match **cond {
+                BExpr::BTrue => Ok(a.clone()),
+                BExpr::BFalse => Ok(b.clone()),
+                _ => cond.eval1(state).map(|cond| if_(cond, a.clone(), b.clone()))
+            },
+            Cmd::While(ref cond, ref c0, ref body) => match **cond {
+                BExpr::BFalse => Ok(skip()),
+                BExpr::BTrue => Ok(seq(body.clone(), while_(c0.clone(), body.clone()))),
+                _ => cond.eval1(state).map(|cond| while_ev(cond, c0.clone(), body.clone()))
+            }
+        }
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    use super::super::state_mocks::*;
+    use super::super::state::*;
+    use super::super::*;
 
     #[test]
     fn test_skip () {
-        let state = MockEmptyState::new();
+        let mut state = HashState::new();
         assert_eq!(true, skip().is_reduced());
+        assert_eq!(Ok(skip()), skip().eval1(&mut state));
+        assert_eq!(true, skip().eval1(&mut state).unwrap().is_reduced());
+        assert_eq!(Ok(skip()), skip().eval1(&mut state).unwrap().eval1(&mut state));
+    }
+    #[test]
+    fn test_assign () {
+        let mut state = HashState::new();
+        let a = assign("x", val(10));
+        let a0 = assign("x", val(10));
+        let a1 = skip();
+
+        assert_eq!(false, a.is_reduced());
+        assert_eq!(true, state.get("x").is_err());
+        assert_eq!(a, a0);
+
+        let b = a.eval1(&mut state).unwrap();
+        assert_eq!(a0, a);
+        assert_eq!(a1, b);
+        assert_eq!(false, a.is_reduced());
+        assert_eq!(true, b.is_reduced());
+        assert_eq!(false, state.is_empty());
+        assert_eq!(Ok(10), state.get("x"));
+    }
+    #[test]
+    fn test_seq () {
+        let mut state = HashState::new();
+        let a = seq(assign("x", val(10)), assign("x", val(20)));
+        let a0 = seq(assign("x", val(10)), assign("x", val(20)));
+        let a1 = seq(skip(), assign("x", val(20)));
+        let a2 = skip();
+
+        assert_eq!(false, a.is_reduced());
+        assert_eq!(true, state.get("x").is_err());
+        assert_eq!(a, a0);
+
+        let b1 = a.eval1(&mut state).unwrap();
+        assert_eq!(a0, a);
+        assert_eq!(a1, b1);
+        assert_eq!(false, a.is_reduced());
+        assert_eq!(false, b1.is_reduced());
+        assert_eq!(Ok(10), state.get("x"));
+
+        let b2 = b1.eval1(&mut state).unwrap();
+        assert_eq!(a0, a);
+        assert_eq!(a1, b1);
+        assert_eq!(a2, b2);
+        assert_eq!(true, b2.is_reduced());
+        assert_eq!(Ok(20), state.get("x"));
+
+        let mut empty_state = HashState::new();
+        let b3 = b2.eval1(&mut empty_state).unwrap();
+        assert_eq!(b2, b3);
+        assert_eq!(Ok(20), state.get("x"));
+        assert_eq!(true, empty_state.get("x").is_err());
     }
 }
--- src/while_lang/ast2/state.rs
+++ src/while_lang/ast2/state.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 
 pub type Value = i64;
 pub type Variable = String;
@@ -5,4 +6,24 @@ pub type VResult = Result<Value, String>;
 pub trait State {
     fn get (&self, var: &str) -> VResult;
     fn set (&mut self, var: &str, val: Value);
+}
+
+// Wrap HashMap but only provide our State's public interface
+pub struct HashState(HashMap<Variable, Value>);
+impl HashState {
+    pub fn new () -> HashState { HashState(HashMap::new()) }
+
+
+    pub fn is_empty (&self) -> bool { self.0.is_empty() }
+}
+impl State for HashState {
+    fn get (&self, v: &str) -> VResult {
+        match self.0.get(v) {
+            Some(a) => Ok(*a),
+            None => Err(format!("Undefined variable '{}'!", v))
+        }
+    }
+    fn set (&mut self, v: &str, a: Value) {
+        self.0.insert(v.to_string(), a);
+    }
 }
