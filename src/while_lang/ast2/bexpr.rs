49735bac4d90657419c858cb2043d423e2616177
Seiji Emery <seijiemery@gmail.com>
2019-05-08 23:51:24 -0700

    added bexpr w/ stubs

---
+++ src/while_lang/ast2/bexpr.rs
@@ -0,0 +1,50 @@
+use std::rc::Rc;
+use super::expr::Expr;
+use super::state::{ State };
+use super::aexpr::{ ARef };
+use super::aexpr;
+
+#[derive(Debug, PartialOrd, PartialEq)]
+enum BExpr {
+    BTrue,
+    BFalse,
+    Not(BRef),
+    Or(BRef, BRef),
+    And(BRef, BRef),
+    Less(ARef, ARef),
+    Equal(ARef, ARef),
+}
+pub type BRef = Rc<BExpr>;
+pub type BResult = Result<BRef, String>;
+
+pub fn btrue () -> BRef { Rc::new(BExpr::BTrue) }
+pub fn bfalse () -> BRef { Rc::new(BExpr::BFalse) }
+pub fn not (a: BRef) -> BRef { Rc::new(BExpr::Not(a)) }
+pub fn or (a: BRef, b: BRef) -> BRef { Rc::new(BExpr::Or(a, b)) }
+pub fn and (a: BRef, b: BRef) -> BRef { Rc::new(BExpr::And(a, b)) }
+pub fn less (a: ARef, b: ARef) -> BRef { Rc::new(BExpr::Less(a, b)) }
+pub fn equal (a: ARef, b: ARef) -> BRef { Rc::new(BExpr::Equal(a, b)) }
+
+impl Expr<bool, BRef> for BRef {
+    fn is_reduced (&self) -> bool {
+        return match **self {
+            BExpr::BTrue => true,
+            BExpr::BFalse => true,
+            _ => false
+        }
+    }
+    fn eval (&self, state: &State) -> Result<bool, String> {
+        Err("unimplemented!".to_string())
+    }
+    fn eval1 (&self, state: &State) -> BResult {
+        Err("unimplemented!".to_string())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use super::super::state_mocks::*;
+
+    // Tests TBD
+}
--- src/while_lang/ast2/mod.rs
+++ src/while_lang/ast2/mod.rs
@@ -2,7 +2,12 @@ mod expr;
 mod state;
 mod state_mocks;
 mod aexpr;
+mod bexpr;
+mod cmd;
 
 use expr::{ Expr };
 use state::{ State, Value, Variable, VResult };
 use aexpr::{ ARef, AResult, val, var, add, sub, mul };
+use bexpr::{ BRef, BResult, btrue, bfalse, not, or, and, less, equal };
+use cmd::{ CRef, CResult, skip, seq, assign, if_, while_ };
+
