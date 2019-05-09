554145f1695a502509be2ab7dcba904bf90528a8
Seiji Emery <seijiemery@gmail.com>
2019-05-08 18:28:17 -0700

    updated bexpr

--- src/while_lang/ast/bexpr.rs
+++ src/while_lang/ast/bexpr.rs
@@ -1,70 +1,41 @@
 use crate::while_lang::types::Value;
 use crate::while_lang::types::State;
 use super::aexpr::AExpr;
+use super::aexpr::ARef;
 //use std::ops::Not;
 //use std::ops::BitOr;
 //use std::ops::BitAnd;
 use std::rc::Rc;
+use std::cell::RefCell;
 use super::aexpr;
 
 #[derive(Debug, PartialOrd, PartialEq)]
 pub enum BExpr {
     Value(bool),
-    Not(Rc<BExpr>),
-    Or(Rc<BExpr>, Rc<BExpr>),
-    And(Rc<BExpr>, Rc<BExpr>),
-    Less(Rc<AExpr>, Rc<AExpr>),
-    Equal(Rc<AExpr>, Rc<AExpr>),
+    Not(BRef),
+    Or(BRef, BRef),
+    And(BRef, BRef),
+    Less(ARef, ARef),
+    Equal(ARef, ARef),
 }
-pub fn evalStep (ast: &mut Rc<BExpr>, state: &State, result: &mut Result<bool, String>) -> bool {
-    return match **ast {
-        BExpr::Value(_) => false,
-        BExpr::Not(ref mut a) => match **a {
-            BExpr::Value(true) => { *ast = bfalse().clone(); true }
-            BExpr::Value(false) => { *ast = btrue().clone(); true }
-            _ => evalStep(a, state, result)
-        },
-        BExpr::Or(ref mut a, ref mut b) => match **a {
-            BExpr::Value(true) => { *ast = a.clone(); true },
-            BExpr::Value(false) => match **b {
-                BExpr::Value(_) => { *ast = b.clone(); true },
-                _ => evalStep(b, state, result)
-            },
-            _ => evalStep(a, state, result)
-        },
-        BExpr::And(ref mut a, ref mut b) => match **a {
-            BExpr::Value(false) => { *ast = a.clone(); true },
-            BExpr::Value(true) => match **b {
-                BExpr::Value(_) => { *ast = b.clone(); true },
-                _ => evalStep(b, state, result)
-            },
-            _ => evalStep(a, state, result)
-        },
-        BExpr::Less(ref mut a, ref mut b) => match **a {
-            AExpr::Value(ref mut a) => match **b {
-                AExpr::Value(ref mut b) => match a < b {
-                    true => { *ast = btrue(); true },
-                    false => { *ast = bfalse(); true },
-                },
-                _ => aexpr::evalStep(b, state, result)
-            },
-            _ => aexpr::evalStep(a, state, result)
-        },
-        BExpr::Equal(ref mut a, ref mut b) => match **a {
-            AExpr::Value(ref mut a) => match **b {
-                AExpr::Value(ref mut b) => match a == b {
-                    true => { *ast = btrue(); true },
-                    false => { *ast = bfalse(); true },
-                },
-                _ => aexpr::evalStep(b, state, result)
-            },
-            _ => aexpr::evalStep(a, state, result)
-        },
-    }
-}
+pub type BRef = Rc<RefCell<BExpr>>;
 
-pub fn eval (ast: &Rc<BExpr>, state: &State) -> Result<bool, String> {
-    return match **ast {
+// constants (reference these, so sort of only defined once...)
+static TRUE : BRef = Rc::new(RefCell::new(BExpr::Value(true)));
+static FALSE : BRef = Rc::new(RefCell::new(BExpr::Value(false)));
+
+// constructors
+pub fn btrue () -> BRef { TRUE.clone() }
+pub fn bfalse () -> BRef { FALSE.clone() }
+pub fn not (a: BRef) -> BRef { Rc::new(RefCell::new(BExpr::Not(a))) }
+pub fn or (a: BRef, b: BRef) -> BRef { Rc::new(RefCell::new(BExpr::Or(a, b))) }
+pub fn and (a: BRef, b: BRef) -> BRef { Rc::new(RefCell::new(BExpr::And(a, b))) }
+pub fn less (a: ARef, b: ARef) -> BRef { Rc::new(RefCell::new(BExpr::Less(a, b))) }
+pub fn equal (a: ARef, b: ARef) -> BRef { Rc::new(RefCell::new(BExpr::Equal(a, b))) }
+
+// eager eval
+pub fn eval (ast: &BRef, state: &State) -> Result<bool, String> {
+    return match *(ast.borrow()) {
         BExpr::Value(v) => Ok(v),
         BExpr::Not(ref a) => eval(a, state).map(|a| !a),
         BExpr::Or(ref a, ref b) => evalBinary(|a, b| a || b, a, b, state),
@@ -73,7 +44,7 @@ pub fn eval (ast: &Rc<BExpr>, state: &State) -> Result<bool, String> {
         BExpr::Equal(ref a, ref b) => evalCmp(|a, b| a == b, a, b, state),
     }
 }
-fn evalBinary <F>(f: F, left: &Rc<BExpr>, right: &Rc<BExpr>, state: &State) -> Result<bool, String>
+fn evalBinary <F>(f: F, left: &BRef, right: &BRef, state: &State) -> Result<bool, String>
     where F: FnOnce(bool, bool) -> bool
 {
     return match eval(left, state) {
@@ -84,7 +55,7 @@ fn evalBinary <F>(f: F, left: &Rc<BExpr>, right: &Rc<BExpr>, state: &State) -> R
         err => err
     };
 }
-fn evalCmp <F>(f: F, left: &Rc<AExpr>, right: &Rc<AExpr>, state: &State) -> Result<bool, String>
+fn evalCmp <F>(f: F, left: &ARef, right: &ARef, state: &State) -> Result<bool, String>
     where F: FnOnce(Value, Value) -> bool
 {
     use super::aexpr;
@@ -97,32 +68,55 @@ fn evalCmp <F>(f: F, left: &Rc<AExpr>, right: &Rc<AExpr>, state: &State) -> Resu
     };
 }
 
-macro_rules!make_ctor {
-    ( $name:ident ($x1:ident: $t1:ident) -> $Container:ident < $Type:ident :: $Tag:ident > ) => {
-        pub fn $name ($x1: $t1) -> $Container<$Type> {
-            return $Container::new($Type::$Tag($x1));
-        }
-    };
-    ( $name:ident ($x1:ident: $t1:ident, $x2:ident: $t2:ident) -> $Container:ident < $Type:ident :: $Tag:ident > ) => {
-        pub fn $name ($x1: $t1, $x2: $t2) -> $Container<$Type> {
-            return $Container::new($Type::$Tag($x1, $x2));
-        }
-    };
+// lazy single-step eval
+pub fn evalStep (ast: &mut BRef, state: &State, result: &mut Result<bool, String>) -> bool {
+    return match *(ast.borrow_mut()) {
+        BExpr::Value(_) => false,
+        BExpr::Not(ref mut a) => match *(a.borrow_mut()) {
+            BExpr::Value(true) => { *ast = bfalse().clone(); true }
+            BExpr::Value(false) => { *ast = btrue().clone(); true }
+            _ => evalStep(a, state, result)
+        },
+        BExpr::Or(ref mut a, ref mut b) => match *(a.borrow_mut()) {
+            BExpr::Value(true) => { *ast = a.clone(); true },
+            BExpr::Value(false) => match *(b.borrow_mut()) {
+                BExpr::Value(_) => { *ast = b.clone(); true },
+                _ => evalStep(b, state, result)
+            },
+            _ => evalStep(a, state, result)
+        },
+        BExpr::And(ref mut a, ref mut b) => match *(a.borrow_mut()) {
+            BExpr::Value(false) => { *ast = a.clone(); true },
+            BExpr::Value(true) => match *(b.borrow_mut()) {
+                BExpr::Value(_) => { *ast = b.clone(); true },
+                _ => evalStep(b, state, result)
+            },
+            _ => evalStep(a, state, result)
+        },
+        BExpr::Less(ref mut a, ref mut b) => match *(a.borrow_mut()) {
+            AExpr::Value(ref mut a) => match *(b.borrow_mut()) {
+                AExpr::Value(ref mut b) => match a < b {
+                    true => { *ast = btrue(); true },
+                    false => { *ast = bfalse(); true },
+                },
+                _ => aexpr::evalStep(b, state, result)
+            },
+            _ => aexpr::evalStep(a, state, result)
+        },
+        BExpr::Equal(ref mut a, ref mut b) => match *(a.borrow_mut()) {
+            AExpr::Value(ref mut a) => match *(b.borrow_mut()) {
+                AExpr::Value(ref mut b) => match a == b {
+                    true => { *ast = btrue(); true },
+                    false => { *ast = bfalse(); true },
+                },
+                _ => aexpr::evalStep(b, state, result)
+            },
+            _ => aexpr::evalStep(a, state, result)
+        },
+    }
 }
 
-type RcedAExpr = Rc<AExpr>;
-type RcedBExpr = Rc<BExpr>;
 
-static TRUE : RcedBExpr = Rc::new(BExpr::Value(true));
-static FALSE : RcedBExpr = Rc::new(BExpr::Value(false));
-
-pub fn btrue () -> RcedBExpr { return TRUE.clone(); }
-pub fn bfalse () -> RcedBExpr { return FALSE.clone(); }
-make_ctor!(not (expr: RcedBExpr) -> Rc<BExpr::Not>);
-make_ctor!(or (left: RcedBExpr, right: RcedBExpr) -> Rc<BExpr::Or>);
-make_ctor!(and (left: RcedBExpr, right: RcedBExpr) -> Rc<BExpr::And>);
-make_ctor!(equal (left: RcedAExpr, right: RcedAExpr) -> Rc<BExpr::Equal>);
-make_ctor!(less (left: RcedAExpr, right: RcedAExpr) -> Rc<BExpr::Less>);
 
 //macro_rules!implement_operator_ctor {
 //    ( $Trait:ident :: $name:ident (
@@ -149,9 +143,9 @@ make_ctor!(less (left: RcedAExpr, right: RcedAExpr) -> Rc<BExpr::Less>);
 //        }
 //    };
 //}
-//implement_operator_ctor!(Not::not (Rc<BExpr>) -> Rc<BExpr::Not>);
-//implement_operator_ctor!(BitAnd::bitand (Rc<BExpr>, Rc<BExpr>) -> Rc<BExpr::And>);
-//implement_operator_ctor!(BitOr::bitor (Rc<BExpr>, Rc<BExpr>) -> Rc<BExpr::Or>);
+//implement_operator_ctor!(Not::not (BRef) -> Rc<BExpr::Not>);
+//implement_operator_ctor!(BitAnd::bitand (BRef, BRef) -> Rc<BExpr::And>);
+//implement_operator_ctor!(BitOr::bitor (BRef, BRef) -> Rc<BExpr::Or>);
 
 // no Equal or Less overloads b/c rust does not support arbitrary operator overloading
 // who on earth would possibly need to use that...? -_-
