3af7404090420a728eee1bf3d13901014a92188e
Seiji Emery <seijiemery@gmail.com>
2019-05-08 18:34:40 -0700

    updated cmd (in progress...)

--- src/while_lang/ast/cmd.rs
+++ src/while_lang/ast/cmd.rs
@@ -2,36 +2,45 @@
 use crate::while_lang::types::Variable;
 use crate::while_lang::types::State;
 use super::aexpr::AExpr;
+use super::aexpr::ARef;
 use super::bexpr::BExpr;
+use super::bexpr::BRef;
 use super::aexpr;
 use super::bexpr;
 use std::rc::Rc;
+use std::cell::RefCell;
 
 #[derive(Debug, PartialOrd, PartialEq)]
 pub enum Cmd {
     Skip,
-    Assign(Variable, Rc<AExpr>),
-    Seq(Rc<Cmd>, Rc<Cmd>),
-    If(Rc<BExpr>, Rc<Cmd>, Rc<Cmd>),
-    While(Rc<BExpr>, Rc<BExpr>, Rc<Cmd>),
+    Assign(Variable, ARef),
+    Seq(CRef, CRef),
+    If(BRef, CRef, CRef),
+    While(BRef, BRef, CRef),
 }
-static SKIP : Rc<Cmd> = Rc::new(Cmd::Skip);
+pub type CRef = Rc<RefCell<Cmd>>;
 
-pub fn skip () -> Rc<Cmd> { SKIP.clone() }
-pub fn assign(v: &str, a: Rc<AExpr>) -> Rc<Cmd> {
-    Rc::new(Cmd::Assign(v.to_string(), a))
+static SKIP : CRef = Rc::new(RefCell::new(Cmd::Skip));
+
+pub fn skip () -> CRef { SKIP.clone() }
+pub fn seq (a: CRef, b: CRef) -> CRef {
+    Rc::new(RefCell::new(Cmd::Seq(a, b)))
 }
-pub fn if_ (cond: Rc<BExpr>, a: Rc<Cmd>, b: Rc<Cmd>) -> Rc<Cmd> {
-    Rc::new(Cmd::If(cond, a, b))
+pub fn assign(v: &str, a: ARef) -> CRef {
+    Rc::new(RefCell::new(Cmd::Assign(v.to_string(), a)))
 }
-pub fn while_ (cond: Rc<BExpr>, body: Rc<Cmd>) -> Rc<Cmd> {
-    Rc::new(Cmd::While(cond, cond.clone(), body))
+pub fn if_ (cond: BRef, a: CRef, b: CRef) -> CRef {
+    Rc::new(RefCell::new(Cmd::If(cond, a, b)))
+}
+pub fn while_ (cond: BRef, body: CRef) -> CRef {
+    Rc::new(RefCell::new(Cmd::While(cond, cond.clone(), body)))
 }
 
-pub fn evalStep (ast: &mut Rc<Cmd>, state: &mut State, result: &mut Result<bool, String>) -> bool {
-    return match **ast {
+// lazy eval (single step)
+pub fn evalStep (ast: &mut CRef, state: &mut State, result: &mut Result<bool, String>) -> bool {
+    return match *(ast.borrow_mut()) {
         Cmd::Skip => false,
-        Cmd::Assign(ref v, ref mut expr) => match **expr {
+        Cmd::Assign(ref v, ref mut expr) => match *(expr.borrow_mut()) {
             AExpr::Value(ref a) => {
                 state.set(v, *a);
                 *ast = skip();
@@ -39,89 +48,23 @@ pub fn evalStep (ast: &mut Rc<Cmd>, state: &mut State, result: &mut Result<bool,
             },
             _ => aexpr::evalStep(expr, state, result)
         },
-        Cmd::Seq(ref mut a, ref mut b) => match **a {
+        Cmd::Seq(ref mut a, ref mut b) => match *(a.borrow_mut()) {
             Cmd::Skip => { *ast = b.clone(); true },
             _ => evalStep(a, state, result)
         },
-        Cmd::If(ref mut cond, ref mut a, ref mut b) => match **cond {
+        Cmd::If(ref mut cond, ref mut a, ref mut b) => match *(cond.borrow_mut()) {
             BExpr::Value(true) => { *ast = a.clone(); true },
             BExpr::Value(false) => { *ast = b.clone(); true },
             _ => bexpr::evalStep(cond, state, result)
         },
-        Cmd::While(ref mut cond, ref mut c0, ref mut body) => match **cond {
+        Cmd::While(ref mut cond, ref mut c0, ref mut body) => match *(cond.borrow_mut()) {
             BExpr::Value(false) => { *ast = skip(); true },
             BExpr::Value(true) => {
                 cond.clone_from(c0);
-                *ast = Rc::new(Cmd::Seq(body.clone(), ast.clone()));
+                *ast = seq(body.clone(), ast.clone());
                 true
             },
             _ => bexpr::evalStep(cond, state, result)
         }
     }
 }
-
-
-
-
-
-//pub fn evalStep (ast: Rc<Cmd>, state: &mut State) -> Result<Rc<Cmd>, String> {
-//    use super::aexpr;
-//    use super::bexpr;
-//    return match *ast {
-//        Cmd::Skip => Ok(ast),
-//        Cmd::Assign(ref v, ref expr) => match *expr {
-//            AExpr::Value(ref a) => {
-//                state.set(v, *a);
-//                Ok(Rc::new(Cmd::Skip))
-//            },
-//            _ => match bexpr::evalStep(*expr, state) {
-//                Ok(a) => Ok(Rc::new(Cmd::Assign(v, Rc::clone(expr)))),
-//            }
-//
-//
-//                Ok(Rc::new(AExpr::Assign()))
-//        },
-//        Cmd::Seq(ref a, ref b) => match *a {
-//            AExpr::Skip => Ok(Rc::clone(b)),
-//            _ => match evalStep(*a, state) {
-//                Ok(a) => Ok(Rc::new(Cmd::Seq(a, Rc::clone(b)))),
-//                err => err
-//            }
-//        },
-//        Cmd::If(ref cond, ref a, ref b) => match *cond {
-//            BExpr::Value(true) => Ok(a),
-//            BExpr::Value(false) => Ok(b),
-//            _ => match bexpr::evalStep(cond, state) {
-//                Ok(cond) => Ok(Rc::new(Cmd::If(cond, a, b))),
-//                Err(msg) => Err(msg)
-//            }
-//        },
-//        Cmd::While(ref cond, ref cmd) => match *cond {
-//            BExpr::Value(false) => Ok(Rc::new(Cmd::Skip)),
-//            BExpr::Value(true) => Ok(Rc::new(Cmd::Seq(cmd, ast))),
-//            _ => match bexpr::evalStep(cond, state) {
-//                Ok(res) => Ok(Rc::new(Cmd::WhileEv(res, cond, cmd))),
-//                Err(err) => Err(err),
-//            }
-//        }
-//        Cmd::WhileEv(ref cond, ref cmd, ref cont) => match *cond {
-//            BExpr::Value(false) => Ok(Rc::new(Cmd::Skip)),
-//
-//
-//
-//            BExpr::Value(ref cond) => {
-//                if (cond) {
-//                    // THIS WILL FAIL!
-//                    // need the ORIGINAL ast, not the evaluated one!
-//                    Ok(Rc::new(Cmd::Seq(cmd, cont)))
-//                } else {
-//                    Ok(Rc::new(Cmd::Skip))
-//                }
-//            },
-//            _ => match bexpr::evalStep(cond, state) {
-//                Ok(cond) => Ok(Rc::new(Cmd::While(cond, a, b)))
-//                Err(err) => Err(err)
-//            }
-//        }
-//    }
-//}
