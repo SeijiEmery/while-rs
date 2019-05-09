a468b4d11ddae14d8032792c72efabc616a9c278
Seiji Emery <seijiemery@gmail.com>
2019-05-09 02:03:19 -0700

    wrote bexpr impl and partial tests

--- src/while_lang/ast2/bexpr.rs
+++ src/while_lang/ast2/bexpr.rs
@@ -1,7 +1,7 @@
 use std::rc::Rc;
 use super::expr::Expr;
 use super::state::{ State };
-use super::aexpr::{ ARef };
+use super::aexpr::{ ARef, AExpr };
 use super::aexpr;
 
 #[derive(Debug, PartialOrd, PartialEq)]
@@ -37,7 +37,47 @@ impl Expr<bool, BRef> for BRef {
         Err("unimplemented!".to_string())
     }
     fn eval1 (&self, state: &State) -> BResult {
-        Err("unimplemented!".to_string())
+        match **self {
+            BExpr::BTrue => Ok(self.clone()),
+            BExpr::BFalse => Ok(self.clone()),
+            BExpr::Not(ref a) => match **a {
+                BExpr::BTrue => Ok(bfalse()),
+                BExpr::BFalse => Ok(btrue()),
+                _ => a.eval1(state).map(|a| not(a))
+            },
+            BExpr::Or(ref a, ref b) => match **a {
+                BExpr::BTrue => Ok(a.clone()),
+                BExpr::BFalse => match **b {
+                    BExpr::BTrue => Ok(b.clone()),
+                    BExpr::BFalse => Ok(b.clone()),
+                    _ => b.eval1(state).map(|b| or(a.clone(), b))
+                }, _ => a.eval1(state).map(|a| or(a, b.clone()))
+            },
+            BExpr::And(ref a, ref b) => match **a {
+                BExpr::BFalse => Ok(a.clone()),
+                BExpr::BTrue => match **b {
+                    BExpr::BTrue => Ok(b.clone()),
+                    BExpr::BFalse => Ok(b.clone()),
+                    _ => b.eval1(state).map(|b| and(a.clone(), b))
+                }, _ => a.eval1(state).map(|a| and(a, b.clone()))
+            },
+            BExpr::Less(ref a, ref b) => match **a {
+                AExpr::Val(xa) => match **b {
+                    AExpr::Val(xb) => match xa < xb {
+                        true => Ok(btrue()),
+                        false => Ok(bfalse()),
+                    }, _ => b.eval1(state).map(|b| less(a.clone(), b))
+                }, _ => a.eval1(state).map(|a| less(a, b.clone()))
+            },
+            BExpr::Equal(ref a, ref b) => match **a {
+                AExpr::Val(xa) => match **b {
+                    AExpr::Val(xb) => match xa == xb {
+                        true => Ok(btrue()),
+                        false => Ok(bfalse()),
+                    }, _ => b.eval1(state).map(|b| equal(a.clone(), b))
+                }, _ => a.eval1(state).map(|a| equal(a, b.clone()))
+            },
+        }
     }
 }
 
@@ -46,5 +86,50 @@ mod tests {
     use super::*;
     use super::super::state_mocks::*;
 
-    // Tests TBD
+    #[test]
+    fn test_true () {
+        let state = MockEmptyState::new();
+        assert_eq!(true, btrue().is_reduced());
+        assert_eq!(Ok(btrue()), btrue().eval1(&state));
+        assert_eq!(true, btrue().eval1(&state).unwrap().is_reduced());
+        assert_eq!(Ok(btrue()), btrue().eval1(&state).unwrap().eval1(&state));
+    }
+    #[test]
+    fn test_false () {
+        let state = MockEmptyState::new();
+        assert_eq!(true, bfalse().is_reduced());
+        assert_eq!(Ok(bfalse()), bfalse().eval1(&state));
+        assert_eq!(true, bfalse().eval1(&state).unwrap().is_reduced());
+        assert_eq!(Ok(bfalse()), bfalse().eval1(&state).unwrap().eval1(&state));
+    }
+    #[test]
+    fn test_not () {
+        let state = MockEmptyState::new();
+        assert_eq!(false, not(btrue()).is_reduced());
+        assert_eq!(false, not(bfalse()).is_reduced());
+        assert_eq!(true, not(btrue()).eval1(&state).unwrap().is_reduced());
+        assert_eq!(true, not(bfalse()).eval1(&state).unwrap().is_reduced());
+        assert_eq!(Ok(bfalse()), not(btrue()).eval1(&state));
+        assert_eq!(Ok(btrue()), not(bfalse()).eval1(&state));
+    }
+    #[test]
+    fn test_or () {
+        let state = MockEmptyState::new();
+        assert_eq!(false, or(btrue(), btrue()).is_reduced());
+        assert_eq!(true, or(btrue(), btrue()).eval1(&state).unwrap().is_reduced());
+        assert_eq!(Ok(btrue()), or(btrue(), btrue()).eval1(&state));
+        assert_eq!(Ok(btrue()), or(bfalse(), btrue()).eval1(&state));
+        assert_eq!(Ok(btrue()), or(btrue(), bfalse()).eval1(&state));
+        assert_eq!(Ok(bfalse()), or(bfalse(), bfalse()).eval1(&state));
+    }
+    #[test]
+    fn test_and () {
+        let state = MockEmptyState::new();
+        assert_eq!(false, and(btrue(), btrue()).is_reduced());
+        assert_eq!(true, and(btrue(), btrue()).eval1(&state).unwrap().is_reduced());
+        assert_eq!(Ok(btrue()), and(btrue(), btrue()).eval1(&state));
+        assert_eq!(Ok(bfalse()), and(bfalse(), btrue()).eval1(&state));
+        assert_eq!(Ok(bfalse()), and(btrue(), bfalse()).eval1(&state));
+        assert_eq!(Ok(bfalse()), and(bfalse(), bfalse()).eval1(&state));
+    }
 }
